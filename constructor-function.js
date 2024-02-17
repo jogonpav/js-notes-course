ffcd9a09f72c5f8a1473228ee5188feebe5e2a66
jogonpav <josepabonvera@gmail.com>
2024-02-16 22:31:33 -0500

    "Creating constructor funtion"

---
+++ constructor-function.js
@@ -0,0 +1,27 @@
+const personalizedMessage = () => 'Greetings in Personalized Message'
+
+function PersonArrowfunction1 (name, ownMessage){
+    this.name = name
+    this.launchMessage = () => ownMessage
+}
+
+function PersonArrowfunction2 (name, ownMessage){
+    this.name = name
+    this.launchMessage = ownMessage
+}
+
+function PersonNormalFunction (name, ownMessage){
+    this.name = name
+    this.launchMessage = function () {
+        return ownMessage
+    } 
+}
+let personNormalFunction = new PersonNormalFunction("jose", "Greetins normal function")
+let personArrowfunction1 = new PersonArrowfunction1("Jhon", "Greetins arrow function 1")
+let personArrowfunction2 = new PersonArrowfunction2("Jhon", personalizedMessage )
+
+personNormalFunction.launchMessage()
+
+console.log (personArrowfunction1.launchMessage())
+
+console.log (personArrowfunction2.launchMessage())
