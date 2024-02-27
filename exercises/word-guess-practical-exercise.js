fa5a66f7ea2afbd78dcc4f1d2c62c476de6bc28d
jogonpav <josepabonvera@gmail.com>
2024-02-26 23:45:46 -0500

    "Added other exercise to practice js concepts"

---
+++ exercises/word-guess-practical-exercise.js
@@ -0,0 +1,28 @@
+//The game consists of guessing a word
+//This game you have attemps to guess a word
+//this 
+correctWord = "apple";
+
+attempts = 3;
+
+console.log(`The word has ${correctWord.length} letters`);
+
+isWordGuessed = false
+
+while(attempts>0 && !isWordGuessed){
+    attempts = attempts - 1;
+    wordInput = window.prompt("Writte a word to guess the correct word")
+    
+    if(wordInput.toLowerCase() === correctWord) {
+        console.log("!!CONGRATULATION!!")
+        console.log("You guess the hide word")
+        isWordGuessed = true
+    }else{
+        console.log("The word was incorrect");
+        console.log(`You still have ${attempts} attempts`)
+    }  
+}
+
+if (attempts === 0 && !isWordGuessed){
+    console.log('!!!GAME OVER!')
+}
